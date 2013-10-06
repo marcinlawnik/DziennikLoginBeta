2f3d0ed6477f571512e97e3dd616b493ca7c325f
Marcin Ławniczak <marcin.safmb@wp.pl>
2013-10-06 11:22:32 +0200

    Removed lorem ipsum

--- index.php
+++ index.php
@@ -76,31 +76,31 @@
 
             <hr>
 
-            <div class="row-fluid marketing">
-                <div class="span6">
-                    <h4>Subheading</h4>
-                    <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+            <!--<div class="row-fluid marketing">-->
+                <!--<div class="span6">-->
+                    <!--<h4>Łatwość użycia</h4>-->
+                    <!--<p>Zarejestruj się raz, resztę zrobi system!</p>-->
 
-                    <h4>Subheading</h4>
-                    <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+                    <!--<h4>Subheading</h4>-->
+                    <!--<p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>-->
 
-                    <h4>Subheading</h4>
-                    <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-                </div>
+                    <!--<h4>Subheading</h4>-->
+                    <!--<p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>-->
+                <!--</div>-->
 
-                <div class="span6">
-                    <h4>Subheading</h4>
-                    <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+                <!--<div class="span6">-->
+                    <!--<h4>Subheading</h4>-->
+                    <!--<p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>-->
 
-                    <h4>Subheading</h4>
-                    <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+                    <!--<h4>Subheading</h4>-->
+                    <!--<p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>-->
 
-                    <h4>Subheading</h4>
-                    <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-                </div>
-            </div>
+                    <!--<h4>Subheading</h4>-->
+                    <!--<p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>-->
+                <!--</div>-->
+            <!--</div>-->
 
-            <hr>
+            <!--<hr>-->
 
             <div class="footer">
                 <p>&copy; Marcin Ławniczak 2013</p>
