092dc455f39c1a45428b27e3bd61a80e75f35524
Marcin Ławniczak <marcin.safmb@wp.pl>
2013-10-06 11:26:27 +0200

    Added image

--- joinBeta.php
+++ joinBeta.php
@@ -163,7 +163,7 @@ if (isset($_POST['isSent']) && $_POST['isSent'] == 'yes') {//check if form has b
                     <!--<li><a href="../userPanel">Panel użytkownika</a></li>-->
                     <!--<li><a href="../contact.php">Kontakt</a></li>-->
                 <!--</ul>-->
-                <h3 class="muted">DziennikLogin</h3>
+                <img src="logo_small.png" style="float: left; margin-right: 20px;"></img><h3 class="muted">DziennikLogin</h3>
             </div>
 
             <hr>
