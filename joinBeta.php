1a80cbbfc8246c1ad3aa0f119051bc7c191bbd1c
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-03-11 16:01:40 +0100

    Update joinBeta.php

--- joinBeta.php
+++ joinBeta.php
@@ -1,8 +1,58 @@
 <?php
-require 'registrationValidation.php';
 require 'bcryptWrapper.php';
 require 'config.local.php';
+//function to check password
+function checkPassword($password, $username = false) {
+    $length = strlen($password);
 
+    if ($length < 8) {
+        return FALSE;
+    } elseif ($length > 32) {
+        return FALSE;
+    } else {
+
+//check for a couple of bad passwords:
+        if ($username && strtolower($password) == strtolower($username)) {
+        return FALSE;
+        } elseif (strtolower($password) == 'password') {
+        return FALSE;
+        } else {
+
+            preg_match_all("/(.)\1{2}/", $password, $matches);
+            $consecutives = count($matches[0]);
+
+            preg_match_all("/\d/i", $password, $matches);
+            $numbers = count($matches[0]);
+
+            preg_match_all("/[A-Z]/", $password, $matches);
+            $uppers = count($matches[0]);
+
+            preg_match_all("/[^A-z0-9]/", $password, $matches);
+            $others = count($matches[0]);
+
+//see if there are 3 consecutive chars (or more) and fail!
+            if ($consecutives > 0) {
+        return FALSE;
+            } elseif ($others > 1 || ($uppers > 1 && $numbers > 1)) {
+//bulletproof
+        return TRUE;
+            } elseif (($uppers > 0 && $numbers > 0) || $length > 14) {
+//very strong
+        return TRUE;
+            } else if ($uppers > 0 || $numbers > 2 || $length > 9) {
+//strong
+        return TRUE;
+            } else if ($numbers > 1) {
+//fair
+        return FALSE;
+            } else {
+//weak
+        return FALSE;
+            }
+        }
+    }
+    return $returns;
+}
 $db_host = $CONF['databaseHost'];
 $db_name = $CONF['databaseName'];
 $db_username = $CONF['databaseUsername'];
