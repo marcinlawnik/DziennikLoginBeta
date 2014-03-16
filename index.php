0acde1b4eec6b84de928faa2b38a6af59fada9cd
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-03-16 11:39:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
         <div class="container-narrow">
 
             <div class="masthead">
-                <img src="logo_small.png" style="float: left; margin-right: 20px;"><h3 class="text-muted" style="float: left;"> DziennikLogin</h3>
+                <img src="logo_small.png" style="float: left; margin-right: 20px;vertical-align:middle;"><h3 class="text-muted" style="float: left;"> DziennikLogin</h3>
             </div>
 
             <div class="jumbotron">
